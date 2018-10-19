9eda370a069614e7dd68d463c5e1b91cbb63d23d
Carson Beus <carson.beus@gmail.com>
2018-10-19 09:58:23 -0600

    added index.html to app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1>Welcome to my flask app</h1>"
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.debug = True
